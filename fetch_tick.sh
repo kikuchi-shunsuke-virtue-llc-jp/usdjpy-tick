82209835462efccae19f2a92f7b35dbbc62e53b5
KIKUCHI Shunsuke <kikuchi.shunsuke@virtue.llc>
2024-07-28 02:35:22 +0900

    first commit

---
+++ fetch_tick.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+
+ccyccy=usdjpy
+datatype=tick
+
+for y in {2007..2023}; do
+	for m in {01..12}; do
+		d=$(date -v"${y}"y -v"${m}"m -v01d -v+1m -v-1d +%d)
+		fn="${ccyccy}-${y}-${m}-01-${y}-${m}-${d}-${datatype}.csv"
+		if [ ! -f "${fn}" ]; then
+			npx dukascopy-node -i ${ccyccy} -from ${y}-${m}-01 -to ${y}-${m}-${d} -t ${datatype} -f csv
+		fi
+	done
+done
